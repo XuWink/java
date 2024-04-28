dcba27ebe1be79b433db94352a70d5c381fffc6b
xuwenke <2036739377@qq.com>
2024-04-28 11:11:43 +0800

    冒泡排序

--- sort/bubble_sort.java
+++ sort/bubble_sort.java
@@ -24,17 +24,19 @@ public class bubble_sort {
     //    优化
     public static void bubbleSort_2(int arr[]) {
         int i, j;
+        int temp = 0;
         for (i = 0; i < arr.length; i++) {
             boolean flag = false;
             for (j = arr.length - 1; j > 0; j--) {
                 if (arr[j - 1] > arr[j]) {
-                    int temp = arr[j];
+                    temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
                     flag = true;
                 }
             }
-            if (!flag){
+            // System.out.println(!flag);
+            if (!flag) {
                 break;
             }
         }
@@ -43,6 +45,8 @@ public class bubble_sort {
 
     public static void main(String[] args) {
         int[] arr = new int[]{2, 4, 1, 5, 7, 0};
+        int[] arr_2 = new int[]{5, 2, 3, 6, 1, 4};
         bubbleSort(arr);
+        bubbleSort_2(arr_2);
     }
 }
