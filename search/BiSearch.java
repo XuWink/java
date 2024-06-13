882f9acfaf4c79dc022fb1d42fc1030a55678cc0
xuwenke <2036739377@qq.com>
2024-06-13 22:12:10 +0800

    二分查找

--- search/BiSearch.java
+++ search/BiSearch.java
@@ -5,7 +5,14 @@ package search;
  * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
  */
 public class BiSearch {
-    public int search(int[] nums, int target) {
+
+    public static void main(String[] args) {
+        int i = search(new int[]{1, 2, 4, 5}, 2);
+        System.out.println(i);
+    }
+
+
+    public static int search(int[] nums, int target) {
 
         int left = 0,right = nums.length - 1;
 
